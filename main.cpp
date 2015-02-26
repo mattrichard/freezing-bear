fcf729859f7e56266464e92dc5b19f119a3691ed
Aubrey Olson <7303829@linux16.mcs.sdsmt.edu>
2015-02-26 11:46:31 -0700

    changed sobel

--- main.cpp
+++ main.cpp
@@ -6,6 +6,33 @@ Assignment 2 for CSC 442
 
 Author: Aubrey Olson // Matt Richard
 Date:   Feb 2015
+Program Description:  This program shows all of the neighborhood processes detailed in class and at
+                        http://www.mcs.sdsmt.edu/csc442/Assignments/PA2.pdf.  It can open, modify,  and
+                        save user-specified images.
+
+Necessary Headers
+    math.h
+    mainwindow.h
+
+Necessary Files:
+    mainwindow.cpp \
+    smoothing3_3.cpp \
+    sharpening.cpp \
+    outofrange.cpp \
+    meanfilterN_N.cpp \
+    outofrangeN_N.cpp \
+    kirsh_direction.cpp \
+    kirsh_magnitude.cpp \
+    laplacian.cpp \
+    maximum.cpp \
+    median_plus.cpp \
+    minimum.cpp \
+    emboss.cpp \
+    median.cpp \
+    standardDevN_N.cpp \
+    sobel_edge_mag.cpp \
+    sobel_edge_dir.cpp \
+    gaussian_smoothing.cpp
 
 Modifications:
 */
